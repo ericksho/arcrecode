bc5661a1328c7809548c8545ea84a79c41aaa40a
Erick  Svec <evsvec@uc.cl>
2014-02-06 17:03:50 -0300

    conversion de codigo a standar codigo = codigo_original+codigo_empresa (wrappeado)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,22 @@
 class Product < ActiveRecord::Base
+
+	def getBarcode
+
+		barcode_value = self.codeNumber.to_s + self.codeText
+		Product.getImg barcode_value
+
+	end
+
+	def self.getImg barcode_value
+		require 'barby'
+		require 'barby/barcode/code_128'
+		require 'barby/outputter/png_outputter'
+		 
+		full_path = "app/assets/images/" + barcode_value + ".png"
+		barcode = Barby::Code128B.new(barcode_value)
+
+		png = barcode.to_png(:margin => 3, :xdim => 1, :height => 55)
+		img = png.to_yaml.gsub('--- !binary |-','')
+	end
 	
 end
