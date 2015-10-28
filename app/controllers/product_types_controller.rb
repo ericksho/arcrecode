a533fa48da4c5f265e69502ed1695b58ef380ce9
Erick Svec <ericksho@gmail.com>
2015-10-28 10:06:16 -0300

    agregado :englishName, al lugar correcto

--- app/controllers/product_types_controller.rb
+++ app/controllers/product_types_controller.rb
@@ -70,6 +70,6 @@ class ProductTypesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_type_params
-      params.require(:product_type).permit(:organic, :code, :name)
+      params.require(:product_type).permit(:organic, :code, :name, :englishName,)
     end
 end
